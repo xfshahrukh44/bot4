09c5be32f3a681b6f617c7fbfad77d8a6c7c753b
xfshahrukh44 <41840869+xfshahrukh44@users.noreply.github.com>
2018-07-29 22:42:28 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,9 @@
-'''
-Project Title: Facebook Birthday Wisher
-Developer: AdityaThakker (Github UserName: @adityathakker) 
-Website: http://www.adityathakker.com
-Github Repo: https://github.com/adityathakker/Facebook-Birthday-Wisher
-'''
+
 from BirthdayWisher import BirthdayWisher
 
-email = "<your email>"
-password = "<your password>"
+email = ""
+password = ""
 
-bw = BirthdayWisher(email=email, password=password)
-bw.wish()
+obj = BirthdayWisher(email=email,password=password)
+obj.wish()
 
